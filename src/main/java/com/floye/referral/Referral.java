2df45b8adc186ae5c7caae14626c66d6aab6741b
floye <Floyedu77@gmail.com>
2025-06-29 16:49:19 +0200

    update

--- src/main/java/com/floye/referral/Referral.java
+++ src/main/java/com/floye/referral/Referral.java
@@ -16,7 +16,7 @@ public class Referral implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	
+
 	@Override
 	public void onInitialize() {
 		// Charger les codes de referral existants
